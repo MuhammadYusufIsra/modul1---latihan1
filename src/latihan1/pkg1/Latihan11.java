72cde361af7d35a115ea69c383756bd4b8c04436
MuhammadYusufIsra <muhammad_yusuf_isra_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 12:15:45 +0700

    modul1 latihan1

---
+++ src/latihan1/pkg1/Latihan11.java
@@ -0,0 +1,24 @@
+//Package
+package latihan1.pkg1;
+
+/**
+ *
+ * @author Yusuf Isra
+ */
+//Class
+public class Latihan11 {
+
+    //Method
+    public static void main(String[] args) {
+    //Deklarasi variabel
+    int bil = 1;
+    //Perulangan do while
+    do{
+      //statement
+      System.out.print(bil+" ");
+      bil++;
+    }while (bil<=10);
+    }
+    }
+    
+
